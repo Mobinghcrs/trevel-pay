99db4b1a2e3077a44303e94d01e56a3b7df24b8c
Mobinghcrs <mobinghcrs@gmail.com>
2025-08-07 23:32:59 +0330

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ export default defineConfig(({ mode }) => {
     base: './', 
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY)    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
