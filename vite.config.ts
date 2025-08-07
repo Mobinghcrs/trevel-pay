20c1b4191a5ea757a4bb4aae7d962e5cecc6cb66
Mobinghcrs <mobinghcrs@gmail.com>
2025-08-07 16:52:23 +0330

    update

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,29 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      server: {
+        port: 3000 // تغییر پورت به ۳۰۰۰
+      },
+      preview: {
+        port: 3000 // تغییر پورت به ۳۰۰۰ برای حالت preview
       }
     };
 });
+// import path from 'path';
+// import { defineConfig, loadEnv } from 'vite';
+
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, '.', '');
+//     return {
+//       define: {
+//         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+//         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+//       },
+//       resolve: {
+//         alias: {
+//           '@': path.resolve(__dirname, '.'),
+//         }
+//       }
+//     };
+// });
